ff901f2f05fda690e3440f78799b8e691ef9ef35
24bsai213arshyas-max <24bsai213arshyas@skacas.ac.in>
2025-09-17 12:22:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Auto year in footer
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Fake send button
+
+document.getElementById("sendBtn").addEventListener("click", function() {
+
+  alert("Your details have been noted!\nName: Arshya S\nEmail: 24bsai213arshyas@skacas.ac.in\nContact: 6382718004");
+
+});
